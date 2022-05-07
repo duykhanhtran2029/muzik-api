d400e6a8005292ba5c5de0356bb5f318af4fb9bd
Kane Tran <kanetran29@gmail.com>
2022-05-07 10:19:40 +0700

    add: DataProcessor

---
+++ DataProcessor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace DataProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName);
+        }
+
+    }
+}
